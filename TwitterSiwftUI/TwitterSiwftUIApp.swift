30357172bc3ab1283f53249ad9fab8a1a694a3d7
Nikita <znv569@gmail.com>
2020-12-28 22:55:10 +0300

    Initial Commit

---
+++ TwitterSiwftUI/TwitterSiwftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSiwftUIApp.swift
+//  TwitterSiwftUI
+//
+//  Created by Admin on 28.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSiwftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
